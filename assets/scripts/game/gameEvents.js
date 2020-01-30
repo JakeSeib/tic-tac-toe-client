126c7d65e7e92fb8a6c449b85ced3c5cd5d8b78e
JakeSeib <jacob.a.seib@gmail.com>
2020-01-30 17:19:10 -0500

    Refactor game space click ui effects
    Quick refactor to shift ui effects of successful click (and subsequent
    shifting of current player in store) into game/ui.js

--- assets/scripts/game/gameEvents.js
+++ assets/scripts/game/gameEvents.js
@@ -5,12 +5,10 @@ const ui = require('./ui')
 const store = require('../store')
 
 const onGameSpaceClick = event => {
-  // change div's text to X or O
   const gameSpaceIndex = event.target.getAttribute('data-cell-index')
   const gameSpaceDiv = $(`[data-cell-index="${gameSpaceIndex}"]`, '.game-board-container')
   if (!['x', 'o'].includes(gameSpaceDiv.text())) {
-    gameSpaceDiv.text(store.players[store.currentPlayerIndex])
-    store.currentPlayerIndex = Math.abs(store.currentPlayerIndex - 1)
+    ui.onGameSpaceClickSuccess(gameSpaceDiv)
   }
 }
 
