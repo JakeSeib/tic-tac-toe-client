72b9626d1b32d6f338e2243080a35e23ae507149
JakeSeib <jacob.a.seib@gmail.com>
2020-01-30 17:08:08 -0500

    Game board fills in with current player
    Game board text changes to 'x' or 'o' depending on current player when
    clicked
    Current player changes on successful click
    Game board is not affected when previously clicked space is clicked

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@
 // const example = require('./example')
 const authEvents = require('./auth/authEvents')
 const gameEvents = require('./game/gameEvents')
+const store = require('./store')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -15,4 +16,6 @@ $(() => {
   $('.change-pw-form').on('submit', authEvents.onChangePw)
   $('.sign-out-form').on('submit', authEvents.onSignOut)
   $('.game-space').on('click', gameEvents.onGameSpaceClick)
+  store.players = ['x', 'o']
+  store.currentPlayerIndex = 0
 })
--- assets/scripts/game/gameEvents.js
+++ assets/scripts/game/gameEvents.js
@@ -2,13 +2,16 @@
 
 const api = require('./api')
 const ui = require('./ui')
+const store = require('../store')
 
 const onGameSpaceClick = event => {
   // change div's text to X or O
   const gameSpaceIndex = event.target.getAttribute('data-cell-index')
-  console.log(gameSpaceIndex)
-  console.log($(`[data-cell-index=${gameSpaceIndex}]`))
-  $(`[data-cell-index=${gameSpaceIndex}]`).text('X')
+  const gameSpaceDiv = $(`[data-cell-index="${gameSpaceIndex}"]`, '.game-board-container')
+  if (!['x', 'o'].includes(gameSpaceDiv.text())) {
+    gameSpaceDiv.text(store.players[store.currentPlayerIndex])
+    store.currentPlayerIndex = Math.abs(store.currentPlayerIndex - 1)
+  }
 }
 
 module.exports = {
