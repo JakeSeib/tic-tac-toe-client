782eaf499a1065f512ef05937cbfe3bd5531ce7c
JakeSeib <jacob.a.seib@gmail.com>
2020-01-31 14:04:10 -0500

    Add new game button
    On main screen, add button to reset game board and start new game

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const authEvents = require('./auth/authEvents')
+const gameEvents = require('./game/gameEvents')
 const store = require('./store')
 
 $(() => {
@@ -10,4 +11,5 @@ $(() => {
   $('.sign-in-form').on('submit', authEvents.onSignIn)
   $('.change-pw-form').on('submit', authEvents.onChangePw)
   $('.sign-out-form').on('submit', authEvents.onSignOut)
+  $('.new-game-btn').on('click', gameEvents.onGameBoardCreate)
 })
